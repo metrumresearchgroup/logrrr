a35bbadff0413753f06d22d7913d05188401dfa3
Devin Pastoor <devin.pastoor@gmail.com>
2018-08-17 22:41:39 -0400

    replace entry using field map

--- R/formatters.R
+++ R/formatters.R
@@ -3,11 +3,18 @@
 Formatter <- R6Class("Formatter",
                      public = list(
                        initialize = function(field_map = NULL) {
+                         private$field_map <- field_map
                        },
                        #' @param entry entry should have, at minimum, fields message, timestamp, level
                        format_entry = function(entry) {
-                         .NotYetImplemented()
+                         if (!is.null(private$field_map)) {
+                           names(entry)[which(names(entry) %in% names(private$field_map))] <- private$field_map
+                         }
+                         entry
                        }
+                     ),
+                     private = list(
+                       field_map = NULL
                      )
 )
 
@@ -18,6 +25,10 @@ TextFormatter <- R6Class("TextFormatter",
                          public = list(
                            initialize = function(...) {
                              super$initialize(...)
+                           },
+                           format_entry = function(entry) {
+                             entry <- super$format_entry(entry)
+                             entry
                            }
                          )
 )
