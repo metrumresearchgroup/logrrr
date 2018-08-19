5b57ca9b92a4508b5981d49f322e450ebfcebed6
Devin Pastoor <devin.pastoor@gmail.com>
2018-08-19 14:39:39 -0400

    accept raw functions as well as expressions for user entries

--- R/entries.R
+++ R/entries.R
@@ -1,9 +1,9 @@
 user_entries <- function(...) {
   lapply(rlang::dots_list(...), function(.x) {
-    if (!rlang::is_bare_formula(.x)) {
-      return(.x)
+    if (rlang::is_bare_formula(.x) || rlang::is_function(.x)) {
+      return(rlang::as_function(.x))
     }
-    return(rlang::as_function(.x))
+    return(.x)
   })
 }
 
@@ -13,7 +13,7 @@ eval_entries <- function(.e) {
   }
   lapply(.e, function(.x) {
     # these should be the entries coerced from formula to function
-    if (rlang::is_closure(.x)) {
+    if (rlang::is_function(.x)) {
       return(.x())
     }
     return(.x)
