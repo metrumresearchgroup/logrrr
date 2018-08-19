7fff4bc41b24923d5fcbe064daa4a0ae8712824e
Devin Pastoor <devin.pastoor@gmail.com>
2018-08-19 19:32:29 -0400

    style and implement setting level

--- R/Logger.R
+++ R/Logger.R
@@ -1,34 +1,55 @@
-
 #' Create a Logrrr
 #' @importFrom R6 R6Class
 #' @name Logrrr
 #' @examples
 NULL
 
+
 #' @export
-Logrrr <- R6Class("Logrrr",
- public =
-   list(
-     initialize = function(output = getOption("logrrr.output_file", stdout())) {
-        # output can be a file or connection
-       private$out <- output
-     },
-     set_output = function() {
-         return(self)
-     },
-     set_formatter = function() {
-         return(self)
-     },
-     set_level = function() {
-         return(self)
-     },
-     with_fields = function() {
-         return(self)
-     },
-     info = function(){
-     }
-   ),
- private = list(
-     out = NULL
- )
+Logrrr <- R6Class(
+  "Logrrr",
+  public = list(
+    fields = NULL,
+    initialize = function(output = getOption("logrrr.output_file", stdout())) {
+      # output can be a file or connection
+      private$out <- output
+    },
+    set_output = function() {
+      return(self)
+    },
+    set_fields = function(...) {
+      self$fields <- user_fields(...)
+      return(self)
+    },
+    set_formatter = function() {
+      return(self)
+    },
+    set_level = function(level) {
+      lvl_set <- NULL
+      if (is.numeric(level)) {
+        lvl_set <- .lvls[[level]]
+      } else if (is.character(level)) {
+        lvl_set <- .lvls[[toupper(level)]]
+      }
+      if (is.null(lvl_set)) {
+        lvl_names <-
+          glue::glue_collapse(glue::glue("{names(.lvls)} or {.lvls}\n\n"))
+        stop(
+          glue::glue(
+            "incompatible level: {level}, should be one of:\n {lvl_names}"
+          )
+        )
+      }
+      self$log_level <- lvl_set
+      return(self)
+    },
+
+    with_fields = function() {
+      return(self)
+    },
+    info = function() {
+
+    }
+  ),
+  private = list(out = NULL)
 )
