dd887114043f8cd54e3b715269d5dca3e9f065cb
Devin Pastoor <devin.pastoor@gmail.com>
2018-08-20 21:55:48 -0400

    add helper and put extra params for write in correct place

--- R/Outputs.R
+++ R/Outputs.R
@@ -1,20 +1,27 @@
-#' stub output
+is_log_output <- function(.x) {
+        if ("LogOutput" %in% class(.x)) {
+                return(TRUE)
+        }
+        return(FALSE)
+}
+
+#' LogOutput base class
 #' @export
 LogOutput <- R6Class("LogOutput",
      public = list(
      format_func = NULL,
      out = NULL,
      initialize = function(format_func = TextFormatter(),
-                           output = stdout(),
-                           append = TRUE,
-                           ...) {
+                           output = stdout()
+                           )
+                            {
         if (!rlang::is_function(format_func)) {
           stop("format_func must be a function")
         }
         self$format_func <- format_func
         self$out <- output
      },
-     write = function(.x) {
+     write = function(.x, ..., append = TRUE) {
         cat(glue::glue("{msg}\n\n", msg = self$format_func(.x)), file = self$out, append = append, ...)
      }
      )
