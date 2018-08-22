7b8657239fa976d22dc7459d538278fc2c1a4d82
Devin Pastoor <devin.pastoor@gmail.com>
2018-08-22 08:51:58 -0400

    update tests to reflect glue class output and new spacing

--- tests/testthat/test-textformatter.R
+++ tests/testthat/test-textformatter.R
@@ -7,20 +7,23 @@ describe("TextFormatter", {
   it("returns entries", {
     tf <- TextFormatter()
     expect_equal(tf(sample_error),
-                 "\033[31mERRO\033[39m something bad happened \033[31mpid\033[39m=23434")
+                 glue::glue("\033[31mERRO\033[39m: something bad happened              \033[31mpid\033[39m=23434"))
     expect_equal(tf(sample_info),
-                 "\033[34mINFO\033[39m success \033[34mpid\033[39m=23434"
+                 glue::glue("\033[34mINFO\033[39m: success                             \033[34mpid\033[39m=23434")
                  )
   })
   it("can suppress colored output", {
     tf <- TextFormatter(no_color = TRUE)
     expect_equal(tf(sample_error),
-                 "ERRO something bad happened pid=23434")
+                 glue::glue("ERRO: something bad happened              pid=23434"))
   })
   it("can keep full level names", {
     tf <- TextFormatter(no_truncate = TRUE, no_color = TRUE)
     expect_equal(tf(sample_error),
-                 "ERROR something bad happened pid=23434")
+                 glue::glue("ERROR: something bad happened              pid=23434"))
+  })
+  it("can suppress extra whitespace for messages", {
+
   })
   it("renames entries in the field map", {
   })
