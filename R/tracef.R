5ad129d3c2f6d977e17461922c451b544a02804b
Devin Pastoor <devin.pastoor@gmail.com>
2018-10-26 00:05:57 -0400

    implement tracing so it works in shiny

--- R/tracef.R
+++ R/tracef.R
@@ -1,12 +1,25 @@
 #' trace the call to file/line
 #' @param .m distinguishing message
 #' @export
-tracef <- function(.m) {
-  sf <- sys.frames()
-  if (is.null(sf[[1]]$srcref)) {
-    return(NULL)
+shinytrace <- function(.m) {
+  sc <- rev(sys.calls()) # start with closest call
+  scc <- as.character(sys.call()) # should be the function/message
+  linenum <- 0
+  srcfile <- "NOSHINY"
+  # tracef("hello") --> "tracef" "hello"
+  scall <- glue::glue("{fn}({msg})", fn = scc[1], msg = glue::double_quote(scc[2]))
+  for (s in sc) {
+    sa <- attr(s, "srcref")
+    if (!is.null(sa)) {
+      if (any(grepl(pattern = scall, fixed = TRUE, x = as.character(sa)))) {
+        if (!is.null(sa[1])) {
+          srcfile <- attr(sa, "srcfile")$filename
+          linenum <- sa[1]
+          break
+        }
+      }
+    }
   }
-  srcfile <- attributes(sf[[1]]$srcref)$srcfile$filename
-  linenum <- grep(sf[[1]]$lines, pattern = sf[[1]]$srcref, fixed = TRUE)
   glue::glue("{srcfile}#{linenum}")
 }
+
